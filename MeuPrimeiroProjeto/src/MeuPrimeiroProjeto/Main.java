5a8818b0eb29fa74b394cb14422919f8325b1e32
AntunesGean <gean.antunes@gmail.com>
2023-03-04 18:09:50 -0300

    add MeuPrimeiroProjeto

---
+++ MeuPrimeiroProjeto/src/MeuPrimeiroProjeto/Main.java
@@ -0,0 +1,17 @@
+package MeuPrimeiroProjeto;
+
+public class Main {
+        public static void main(String[] args) {
+            System.out.println("Olá Mundo!");
+
+            // comentário de uma linha
+
+            /*
+                Comentário de várias linhas(bloco)
+            */
+
+            /**
+                Comentário de documentação
+            */
+        }
+}
